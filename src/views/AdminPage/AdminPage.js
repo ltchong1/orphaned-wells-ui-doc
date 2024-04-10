db4f7b2962c0d3282cf38535f50ea1b48aed4135
MichaelPesce <michaelcpesce@gmail.com>
2024-04-10 05:33:12 -0500

    update function call to include user role

--- src/views/AdminPage/AdminPage.js
+++ src/views/AdminPage/AdminPage.js
@@ -39,7 +39,7 @@ export default function AdminPage() {
     }
 
     useEffect(()=> {
-        callAPI(getUsers, [], handleAuthSuccess, handleAuthError)
+        callAPI(getUsers, ["base_user"], handleAuthSuccess, handleAuthError)
     },[])
 
     useEffect(()=> {
