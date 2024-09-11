9b4ce94ff914777413b2a7692c529ad17870bc4a
MichaelPesce <michaelcpesce@gmail.com>
2024-09-11 13:51:31 -0400

    add initial cypress test setup

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,5 @@
+describe('End to end testing', () => {
+  it('passes', () => {
+    cy.visit('/')
+  })
+})
---
+++ cypress/support/commands.ts
@@ -0,0 +1,6 @@
+/// <reference types="cypress" />
+// ***********************************************
+// For comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
