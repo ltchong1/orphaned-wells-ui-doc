20db6e6f507b93784b753eeb74bca448f07f3d95
MichaelPesce <michaelcpesce@gmail.com>
2024-09-13 11:41:31 -0400

    add login id

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,8 +1,11 @@
-import '@types/jest';
-import "cypress"
+// import 'jest';
+// import "cypress"
 
 describe('End to end testing', () => {
   it('passes', () => {
-    cy.visit('/')
+    cy.visit('/');
+    cy.wait(1000);
+    cy.get('#login-button').click();
+    cy.wait(50000)
   })
 })
