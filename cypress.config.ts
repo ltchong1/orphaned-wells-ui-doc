09f3b567e2e59b6d4677a42c87b956f7cd5b8c79
MichaelPesce <michaelcpesce@gmail.com>
2024-09-23 09:32:14 -0400

    stagingurl -> backendurl

--- cypress.config.ts
+++ cypress.config.ts
@@ -15,6 +15,6 @@ export default defineConfig({
     googleRefreshToken: process.env.GOOGLE_REFRESH_TOKEN,
     googleClientId: process.env.REACT_APP_GOOGLE_CLIENTID,
     googleClientSecret: process.env.REACT_APP_GOOGLE_CLIENT_SECRET,
-    backendURL: process.env.REACT_APP_STAGING_BACKEND_URL
+    backendURL: process.env.REACT_APP_BACKEND_URL
   },
 });
