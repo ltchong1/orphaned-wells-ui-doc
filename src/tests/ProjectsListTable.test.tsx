57912ae7cbd4702548db299e19563f66037714ed
MichaelPesce <michaelcpesce@gmail.com>
2024-09-26 09:05:38 -0400

    move mockprojects to separate file for component tests

--- src/tests/ProjectsListTable.test.tsx
+++ src/tests/ProjectsListTable.test.tsx
@@ -1,35 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import ProjectsListTable from "../components/ProjectsListTable/ProjectsListTable"
 import { HashRouter } from "react-router-dom";
+import mockProjects from './mockProjects.json'
 
-const mockProjects = [
-    {
-        "id_": "65a1682caf3fee6e0acd211f",
-        "name": "Test Project",
-        "description": "This is a Colorado project",
-        "state": "CO",
-        "creator": {
-            "name": "test"
-        },
-        "history": [
-            ""
-        ],
-        "attributes": []
-    },
-    {
-        "id_": "65a168bcaf3fee6e0acd2120",
-        "name": "Test Project 2",
-        "description": "This is a California project",
-        "state": "CA",
-        "creator": {
-            "name": "test"
-        },
-        "history": [
-            ""
-        ],
-        "attributes": []
-    }
-]
 
 test('test project list table', (): void => {
 
