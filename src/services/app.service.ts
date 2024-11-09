9fc45fec55a6b7db3992a62c83c77f19965e9dd7
MichaelPesce <michaelcpesce@gmail.com>
2024-11-09 10:51:37 -0600

    add optional reprocessed query parameter

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -91,8 +91,9 @@ export const addRecordGroup = (data: any) => {
     });
 };
 
-export const uploadDocument = (data: FormData, project_id: string)  => {
-    return fetch(BACKEND_URL + '/upload_document/' + project_id + '/' + localStorage.getItem("user_email"), {
+export const uploadDocument = (data: FormData, project_id: string, reprocessed?: boolean)  => {
+    if (!reprocessed) reprocessed = false
+    return fetch(BACKEND_URL + '/upload_document/' + project_id + '/' + localStorage.getItem("user_email")+'?reprocessed='+reprocessed, {
         method: 'POST',
         mode: 'cors',
         body: data,
