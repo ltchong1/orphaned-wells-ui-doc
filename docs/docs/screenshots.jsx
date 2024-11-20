74fd2bd0d4dae2e4e85d52f4ccfb2e28cc15d4cf
Dan Gunter <dkgunter@lbl.gov>
2024-11-19 21:10:54 -0800

    first pass at guide page

---
+++ docs/docs/screenshots.jsx
@@ -0,0 +1,21 @@
+import ProjectExportUrl from '@site/static/screenshots/project-export.png';
+import ProjectListUrl from '@site/static/screenshots/project-list.png';
+import ProjectReportsUrl from '@site/static/screenshots/project-reports.png';
+import ProjectReviewUrl from '@site/static/screenshots/project-review.png';
+import ProjectViewUrl from '@site/static/screenshots/project-view.png';
+
+export function ProjectExportImg() {
+    return <img src={ProjectExportUrl} />;
+}
+export function ProjectListImg() {
+    return <img src={ProjectListUrl} />;
+}
+export function ProjectReportsImg() {
+    return <img src={ProjectReportsUrl} />;
+}
+export function ProjectReviewImg() {
+    return <img src={ProjectReviewUrl} />;
+}
+export function ProjectViewImg() {
+    return <img src={ProjectViewUrl} />;
+}
