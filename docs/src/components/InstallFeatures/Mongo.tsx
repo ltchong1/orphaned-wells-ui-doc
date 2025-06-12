9816374d666e47fa661efc5a4512862b2d1d27c7
MichaelPesce <michaelcpesce@gmail.com>
2025-06-12 11:38:16 -0500

    add more to docs; add section header for adding users; need script to initialize mongodb

--- docs/src/components/InstallFeatures/Mongo.tsx
+++ docs/src/components/InstallFeatures/Mongo.tsx
@@ -12,15 +12,28 @@ export function HowToSetupMongo() {
     <div>
       <ol>
         <li><p>
-          MongoDB can be set up locally or in the cloud. <a target='_blank' href="https://www.mongodb.com/products/platform/atlas-database">MongoDB Atlas</a> allows users to create one free tier database cluster with the cloud provider of your choice. OGRRE depends on GCP for other cloud services, so it likely makes the most sense to use GCP. 
+          MongoDB can be set up locally or in the cloud. <a target='_blank' href="https://www.mongodb.com/products/platform/atlas-database">MongoDB Atlas</a> allows users to create one free tier database cluster with the cloud provider of your choice. OGRRE depends on GCP for other cloud services, so if using Atlas, it likely makes the most sense to use GCP. 
         </p></li>
         <li><p>
-          To create a project and your free cluster, see the <a target='_blank' href="https://www.mongodb.com/docs/atlas/government/tutorial/create-project/">MongoDB documentation</a>. The documentation also provides instructions on how to <a target='_blank' href="https://www.mongodb.com/docs/atlas/government/tutorial/create-mongodb-user-for-cluster/">create a database user</a> and <a target='_blank' href="https://www.mongodb.com/docs/atlas/tutorial/connect-to-your-cluster/">connect to your database</a>.
+          To create a project and your free cluster, see the <a target='_blank' href="https://www.mongodb.com/docs/atlas/government/tutorial/create-project/">MongoDB documentation</a>. The documentation also provides instructions on how to <a target='_blank' href="https://www.mongodb.com/docs/atlas/government/tutorial/create-mongodb-user-for-cluster/">create a database user</a> and <a target='_blank' href="https://www.mongodb.com/docs/atlas/tutorial/connect-to-your-cluster/">connect to your database</a>. To connect your database to OGRRE, you will need to create a database user.
         </p></li>
         <li><p>
-          To connect to the database, you must also ensure that the IP address you want to connect from is added to the access list. In the case of OGRRE, the backend IP address must be added to this list to allow it to access and update the database records. For information on how to add an IP address, see <a target='_blank' href="See https://www.mongodb.com/docs/atlas/security/ip-access-list/. ">here</a>.
+          To connect to the database, you must also ensure that the IP address you want to connect from is added to the access list. In the case of OGRRE, the backend IP address must be added to this list to allow it to access and update the database records. For information on how to add an IP address, see <a target='_blank' href="https://www.mongodb.com/docs/atlas/security/ip-access-list/. ">here</a>.
         </p></li>
       </ol>
+    </div>
+  );
+}
+
+export function InitializeDatabase() {
+  return (
+    <div>
+      OGRRE stores teams, authorized users, and assigned roles/permissions in the MongoDB database. So in order for OGRRE to run with a new database, some initial collections and documents must be created. The collections <code>teams</code>, <code>users</code>, and <code>roles</code> must be created before running OGRRE. There must be at least one team and one user associated with that team, as well as a list of roles that are specific to OGRRE. You can create these manually, or use the python script we have provided to create these documents for you. The script is called <code>initializeMongoDB.py</code>, and can be downloaded <a target="_blank">here TODO: add link</a>. The script takes the following arguments:
+      <ul>
+        <li><b>team_name</b> (<b>-t</b>): the initial team name [<i>required</i>]</li>
+        <li><b>email</b> (<b>-e</b>): email of the initial user [<i>required</i>]</li>
+        <li><b>assigned_role</b> (<b>-a</b>): The role for the user you are creating. If not provided, the user will be assigned the role of <code>sys_admin</code> which contains all system and team permissions [<i>optional</i>]</li>
+      </ul>
     </div>
   );
 }
