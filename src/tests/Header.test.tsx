87cd746e53c3bf572f6222c4f499db3c8fa45429
ltchong1 <leebynchong@gmail.com>
2025-08-04 14:34:57 -0400

    Deploy website - based on 9b123c45c51650aa4fd205539b0124e05fbac3f4

--- src/tests/Header.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Header from "../components/Header/Header";
-import { HashRouter } from "react-router-dom";
-
-test('test header', (): void => {
-    render(<HashRouter><Header /></HashRouter>);
-
-});
